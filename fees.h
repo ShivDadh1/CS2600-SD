758cd8e73adc2e6b00e5dbc156d9b60bc2a53390
Shivam Dadhania <smdadhania@cpp.edu>
2022-03-07 18:31:20 -0800

    Updated fees header

---
+++ fees.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifndef ConfParkTaxiHotelFeesHeader_H
+
+#define ConfParkTaxiHotelFeesHeader_H
+
+float numOfdays;
+float parkingFee,taxiFee; 
+
+float conferenceFees(float seminarFees);
+void parkingFees(float *parkFee, float numOfDays);
+void taxiFees(const char vehicleType[], float *taxiFee, float numOfDays);
+
+#endif
